6d914a6d0e3ad96d77fca9c77d28782c1cd4477c
lausek <root@lausek.eu>
2020-09-09 22:47:23 +0200

    implement start command

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # digital-asm-headless
+
+A python script to control [Digital](https://github.com/hneemann/Digital).
+
+## Setup
+
+```
+export DIGASMJAR=<path_to_assembler_jar>
+```
--- src/__main__.py
+++ src/__main__.py
@@ -1,28 +1,59 @@
 #!/usr/bin/python3
 
 import argparse
+import os
+import os.path
 import socket
 import struct
+import subprocess
 
+JAR_ENVVAR = 'DIGASMJAR'
 HOST, PORT = 'localhost', 41114
 
 def pack(msg):
     buf = msg.encode('utf8')
-    print(len(buf))
     n = struct.pack('>H', len(buf))
-    print(n)
     return n + buf
 
-def main():
+def trigger(evt, arg=None):
     for fam, ty, proto, flags, host in socket.getaddrinfo(HOST, PORT):
         with socket.socket(fam, ty) as s:
             s.connect(host[:2])
 
-            msg = 'start:<path>'
+            if arg is None:
+                msg = str(evt)
+            else:
+                msg = '{}:{}'.format(evt, arg)
             msg = pack(msg)
 
             if s.sendall(msg) is None:
                 break
 
+def hexup(fname):
+    jar_location = os.environ[JAR_ENVVAR]
+    subprocess.run(['java', '-jar', jar_location, fname], check=True)
+
+def start(args):
+    fname = args.file
+    hexup(fname)
+    hexname = os.path.splitext(fname)[0] + '.hex'
+    trigger('start', hexname)
+
 if __name__ == '__main__':
-    main()
+    if not JAR_ENVVAR in os.environ:
+        print('environment variable', JAR_ENVVAR, 'not set')
+        exit()
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    start_parser = subparsers.add_parser('start')
+    start_parser.add_argument('file', type=str)
+    start_parser.set_defaults(func=start)
+
+    args = parser.parse_args()
+
+    try:
+        args.func(args)
+    except AttributeError:
+        parser.print_help()
